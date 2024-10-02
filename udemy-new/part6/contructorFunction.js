1be5f576272222e93714bd05be08ba6874bf6245
mohit9102 <always.mohitkr@gmail.com>
2024-10-03 02:01:53 +0530

    oops concept in java

--- udemy-new/part6/contructorFunction.js
+++ udemy-new/part6/contructorFunction.js
@@ -1,47 +1,146 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+
+// function greet(name){
+
+//   console.log("Hello", name);
+
+// }
+
+function Person(name, age){
+
+     this.name = name
+     this.age = age
+     
 }
 
-function Car(make, model) {
-  this.make = make;
-  this.model = model;
+function Car(make, model){
+  this.make = make
+  this.model = model
 }
 
-let myCar = new Car("Toyota", "Camry");
+
+let myCar = new Car("Toyota", "camry")
 // console.log(myCar);
 
-let myNewCar = Car("Tata", "Safari");
+let myNewCar = new Car("Tata", "safari")
 // console.log(myNewCar);
 
-function Tea(type) {
-  this.type = type;
-  this.describe = function () {
-    return `this is a cup of ${this.type}`;
-  };
+
+function Tea(type){
+     this.type = type
+     this.describe = function(){
+         return `this a cup of ${this.type}`
+     }
 }
-let lemonTea = new Tea("lemon tea");
+
+let lemonTea = new Tea("lemon tea ")
 // console.log(lemonTea.describe());
 
-function Animal(species) {
-  this.species = species;
+
+
+
+
+
+
+function Animal(species){
+     this.species = species
+
+
 }
 
-Animal.prototype.sound = function () {
-  return `${this.species} makes a sound`;
-};
 
-let dog = new Animal("Dog");
-// console.log(dog.sound());
-
-let cat = new Animal("cat");
-// console.log(cat.sound());
-
-function Drink(name) {
-  if (!new.target) {
-    throw new Error("Drink must be called with new keyword");
-  }
-  this.name = name;
+Animal.prototype.sound = function(){
+     return `${this.species} make a sound`
 }
-let tea = new Drink("tea");
-let coffee = Drink("coffee");
+
+let dog = new Animal("dog");
+console.log(dog.sound());
+
+
+
+function Drink(name){
+     if(!new.target){
+          throw new Error("Drink must be called with new keyword");
+          
+     }
+     this.name = name
+}
+
+let tea = new drink("tea")
+let coffee = Drink("cofee")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// function Car(make, model) {
+//   this.make = make;
+//   this.model = model;
+// }
+
+// let myCar = new Car("Toyota", "Camry");
+// // console.log(myCar);
+
+// let myNewCar = Car("Tata", "Safari");
+// // console.log(myNewCar);
+
+// function Tea(type) {
+//   this.type = type;
+//   this.describe = function () {
+//     return `this is a cup of ${this.type}`;
+//   };
+// }
+// let lemonTea = new Tea("lemon tea");
+// // console.log(lemonTea.describe());
+
+// function Animal(species) {
+//   this.species = species;
+// }
+
+// Animal.prototype.sound = function () {
+//   return `${this.species} makes a sound`;
+// };
+
+// let dog = new Animal("Dog");
+// // console.log(dog.sound());
+
+// let cat = new Animal("cat");
+// // console.log(cat.sound());
+
+// function Drink(name) {
+//   if (!new.target) {
+//     throw new Error("Drink must be called with new keyword");
+//   }
+//   this.name = name;
+// }
+// let tea = new Drink("tea");
+// let coffee = Drink("coffee");
--- udemy-new/part6/prototypes.js
+++ udemy-new/part6/prototypes.js
@@ -1,18 +1,51 @@
-let computer = { cpu: 12 };
+let computer = { cpu : 12};
 let lenovo = {
-  screen: "HD",
-  __proto__: computer,
-};
+   screen : "HD",
+   __proto__: computer //that means i want to acsses the property 
+   //present in computer object
+  
+  };
 let tomHardware = {};
 
-// console.log(`lenovo `, lenovo.__proto__);
 
-let genericCar = { tyres: 4 };
+// console.log(computer);
+
+// console.log(`computer` , computer.__proto__);///this is the most common way of accessing.
+// So you want to access any prototype
+// for any particular object or anything.
+
+// console.log(`lenovo`, lenovo.__proto__);
+// console.log(lenovo.cpu);//
+
+
+
+////////////// ++++++++++ mordern of accesing the other object property
+//////////////////// JavaScript uses prototype-based inheritance to allow objects to share behavior.
+
+
+let genericCar = { tyres: 4}
 
 let tesla = {
-  driver: "AI",
-};
+  driver : "AI"
+}
 
-Object.setPrototypeOf(tesla, genericCar);
 
-console.log(`tesla `, Object.getPrototypeOf(tesla));
+Object.setPrototypeOf(tesla, genericCar)
+
+// console.log(`tesla`, tesla.tyres );
+console.log(`tesla`, Object.getPrototypeOf(tesla));
+// Tesla always have access to its own property.
+// But whatever the property you have injected, if
+// you want to have an access of this,
+// try to access them by get prototype off.
+
+
+
+
+
+
+
+
+
+
+
