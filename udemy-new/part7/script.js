c48b84208380d17620f99e7827b262d894233763
mohit9102 <always.mohitkr@gmail.com>
2024-10-05 01:52:08 +0530

    dom and bom

---
+++ udemy-new/part7/script.js
@@ -0,0 +1,24 @@
+console.log("Hello from script");
+// console.log(window);
+
+
+// const result = window.confirm("Are you sure?");
+// if (result) {
+//     console.log("User pressed OK");
+// } else {
+//     console.log("User pressed Cancel");
+// }
+// const userInput = window.prompt("Please enter your name:");
+// console.log(userInput);
+// const newWindow = window.open("https://www.google.com", "_blank", "width=500,height=500");
+// newWindow.close();  // Closes the new window
+
+// window.onload = () => {
+//     console.log("Page is fully loaded!");
+// };
+
+console.log(document);
+
+
+
+
